c7768100241937023fb04cc63f0fd5252d62a2ed
NAMAN BAJPAYI <37713929+namanbajpayi@users.noreply.github.com>
2020-10-10 15:38:39 +0530

    v1
    first sample

---
+++ upload_files_to_s3.py
@@ -0,0 +1,23 @@
+'''libraries'''
+import boto3
+from botocore.client import Config
+
+'''inputs for file and bucket name'''
+print("hey!! kindly provide the file name that you want to upload on s3: ",end="")
+str=input()
+print("kindly provide the s3 Bucket name: ",end="")
+BUCKET_NAME=input() 
+
+'''Credentials'''
+ACCESS_KEY = 'AKIA2MFMEKAGSNYMQEWZ'
+SECRET_KEY = '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'
+           
+'''Opening binary File'''
+data = open(str, 'rb')
+    
+'''Accessing the bucket'''
+s3 = boto3.resource( 's3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY,config=Config(signature_version='s
+3v4')) 
+s3.Bucket(BUCKET_NAME).put_object(Key=str, Body=data)
+    
+print ("Successfully Uploaded")
